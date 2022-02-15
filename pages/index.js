aa05eef744965aa59b3f40848b6b50236c1ef13a
TIS-24 <shubham.kale@techinfini.com>
2022-02-15 14:12:24 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Heading, Page } from "@shopify/polaris";
 
 const Index = () => (
   <Page>
-    <Heading>Shopify app with Node and React 11 new code update here..............🎉</Heading>
+    <Heading>Shopify app with Node and React 11 new code update here.............. test detail added...</Heading>
   </Page>
 );
 
